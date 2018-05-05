6262cd66223eb7a3cea67edc7ad79b37862c9617
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-05 17:28:54 +0200

    [INIT] init ci

---
+++ app/src/main/java/com/example/jeroj/kenkenjj/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jeroj.kenkenjj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
