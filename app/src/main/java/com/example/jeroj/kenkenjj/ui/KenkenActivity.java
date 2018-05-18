394881793950f903629d07b6d3a125e4196493ea
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-18 17:42:44 +0200

    [EVOL] bouton nouvelle partie sur ecran grille

--- app/src/main/java/com/example/jeroj/kenkenjj/ui/KenkenActivity.java
+++ app/src/main/java/com/example/jeroj/kenkenjj/ui/KenkenActivity.java
@@ -17,7 +17,11 @@ import java.util.ArrayList;
 public class KenkenActivity extends ActivityBase {
     private GridView gridView;
     private Button raz_btn;
+    private Button new_game_btn;
     private BlockAdapter blockAdapter;
+
+
+    private Boolean test_id_grille = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,51 +31,99 @@ public class KenkenActivity extends ActivityBase {
         bindView();
     }
 
-    private ArrayList<Block> get_blocs() {
+    private ArrayList<Block> get_blocs(Integer new_grille) {
         ArrayList<Block> blocks = new ArrayList<>();
-        //1
-        blocks.add(new Block( "15x", true, true, false, true, 5));
-        blocks.add(new Block( "", true, false, true, false, 1));
-        blocks.add(new Block( "10x", true, true, true, false, 2));
-        blocks.add(new Block( "24x", true, true, true, false, 4));
-        blocks.add(new Block( "11+",true, true, false, true, 3));
-        blocks.add(new Block( "",true, false, true, false, 6));
-        //2
-        blocks.add(new Block( "2/", true, true, true, false, 4));
-        blocks.add(new Block( "", false, true,true,true, 3));
-        blocks.add(new Block( "", false, true,true,true, 5));
-        blocks.add(new Block( "", false, true, false, true, 1));
-        blocks.add(new Block( "", true, false, true, true, 6));
-        blocks.add(new Block( "", false, true,true,true, 2));
-        //3
-        blocks.add(new Block( "", false, true,true,true, 2));
-        blocks.add(new Block( "5-", true, true, false, true, 6));
-        blocks.add(new Block( "", true, false, true, true, 1));
-        blocks.add(new Block( "9+", true, true, false, true, 5));
-        blocks.add(new Block( "", true, false, true, true, 4));
-        blocks.add(new Block( "60x", true, true, true, false, 3));
-        //4
-        blocks.add(new Block( "2/", true, true, true, false, 3));
-        blocks.add(new Block( "3-", true, true, true, false, 5));
-        blocks.add(new Block( "2-", true, true, true, false, 6));
-        blocks.add(new Block( "3+", true, true, false, true, 2));
-        blocks.add(new Block( "", true, false, true, true, 1));
-        blocks.add(new Block( "", false, true, true, false, 4));
-        //5
-        blocks.add(new Block( "", false, true,true,true, 6));
-        blocks.add(new Block( "",false, true, true, true, 2));
-        blocks.add(new Block( "",false, true, true, true, 4));
-        blocks.add(new Block( "3-", true, true, true, false, 3));
-        blocks.add(new Block( "", true, true, false, true, 5));
-        blocks.add(new Block( "", false, false, true, true, 1));
-        //6
-        blocks.add(new Block( "12x", true, true, false, true, 1));
-        blocks.add(new Block( "", true, false, false, true, 4));
-        blocks.add(new Block( "", true, false, true, true, 3));
-        blocks.add(new Block( "", false, true,true,true, 6));
-        blocks.add(new Block( "3-", true, true, false, true, 2));
-        blocks.add(new Block( "", true, false, true, true, 5));
 
+        if(new_grille == 1 ) {
+            this.test_id_grille = !this.test_id_grille;
+        }
+        if(this.test_id_grille) {
+            //1
+            blocks.add(new Block("15x", true, true, false, true, 5));
+            blocks.add(new Block("", true, false, true, false, 1));
+            blocks.add(new Block("10x", true, true, true, false, 2));
+            blocks.add(new Block("24x", true, true, true, false, 4));
+            blocks.add(new Block("11+", true, true, false, true, 3));
+            blocks.add(new Block("", true, false, true, false, 6));
+            //2
+            blocks.add(new Block("2/", true, true, true, false, 4));
+            blocks.add(new Block("", false, true, true, true, 3));
+            blocks.add(new Block("", false, true, true, true, 5));
+            blocks.add(new Block("", false, true, false, true, 1));
+            blocks.add(new Block("", true, false, true, true, 6));
+            blocks.add(new Block("", false, true, true, true, 2));
+            //3
+            blocks.add(new Block("", false, true, true, true, 2));
+            blocks.add(new Block("5-", true, true, false, true, 6));
+            blocks.add(new Block("", true, false, true, true, 1));
+            blocks.add(new Block("9+", true, true, false, true, 5));
+            blocks.add(new Block("", true, false, true, true, 4));
+            blocks.add(new Block("60x", true, true, true, false, 3));
+            //4
+            blocks.add(new Block("2/", true, true, true, false, 3));
+            blocks.add(new Block("3-", true, true, true, false, 5));
+            blocks.add(new Block("2-", true, true, true, false, 6));
+            blocks.add(new Block("3+", true, true, false, true, 2));
+            blocks.add(new Block("", true, false, true, true, 1));
+            blocks.add(new Block("", false, true, true, false, 4));
+            //5
+            blocks.add(new Block("", false, true, true, true, 6));
+            blocks.add(new Block("", false, true, true, true, 2));
+            blocks.add(new Block("", false, true, true, true, 4));
+            blocks.add(new Block("3-", true, true, true, false, 3));
+            blocks.add(new Block("", true, true, false, true, 5));
+            blocks.add(new Block("", false, false, true, true, 1));
+            //6
+            blocks.add(new Block("12x", true, true, false, true, 1));
+            blocks.add(new Block("", true, false, false, true, 4));
+            blocks.add(new Block("", true, false, true, true, 3));
+            blocks.add(new Block("", false, true, true, true, 6));
+            blocks.add(new Block("3-", true, true, false, true, 2));
+            blocks.add(new Block("", true, false, true, true, 5));
+        } else {
+            //1
+            blocks.add(new Block("1-", true, true, true, false, 4));
+            blocks.add(new Block("150x", true, true, false, false, 6));
+            blocks.add(new Block("", true, false, true, true, 5));
+            blocks.add(new Block("2/", true, true, false, true, 2));
+            blocks.add(new Block("", true, false, true, true, 1));
+            blocks.add(new Block("2-", true, true, true, false, 3));
+            //2
+            blocks.add(new Block("", false, true, true, true, 3));
+            blocks.add(new Block("", false, true, true, true, 5));
+            blocks.add(new Block("1-", true, true, true, false, 4));
+            blocks.add(new Block("3/", true, true, false, true, 6));
+            blocks.add(new Block("", true, false, true, true, 2));
+            blocks.add(new Block("", false, true, true, true, 1));
+            //3
+            blocks.add(new Block("1-", true, true, false, true, 1));
+            blocks.add(new Block("", true, false, true, true, 2));
+            blocks.add(new Block("", false, true, true, true, 3));
+            blocks.add(new Block("4-", true, true, true, false, 5));
+            blocks.add(new Block("2-", true, true, false, true, 6));
+            blocks.add(new Block("", true, false, true, true, 4));
+            //4
+            blocks.add(new Block("10+", true, true, false, true, 5));
+            blocks.add(new Block("", true, false, false, true, 3));
+            blocks.add(new Block("", true, false, true, true, 2));
+            blocks.add(new Block("", false, true, true, true, 1));
+            blocks.add(new Block("1-", true, true, true, false, 4));
+            blocks.add(new Block("6+", true, true, true, true, 6));
+            //5
+            blocks.add(new Block("3/", true, true, true, false, 6));
+            blocks.add(new Block("3-", true, true, true, false, 4));
+            blocks.add(new Block("5-", true, true, true, false, 1));
+            blocks.add(new Block("36x", true, true, true, false, 3));
+            blocks.add(new Block("", false, true, true, true, 5));
+            blocks.add(new Block("3-", true, true, true, false, 2));
+            //6
+            blocks.add(new Block("", false, true, true, true, 2));
+            blocks.add(new Block("", false, true, true, true, 1));
+            blocks.add(new Block("", false, true, true, true, 6));
+            blocks.add(new Block("", false, true, false, true, 4));
+            blocks.add(new Block("", true, false, true, true, 3));
+            blocks.add(new Block("", false, true, true, true, 5));
+        }
         return blocks;
     }
 
@@ -96,23 +148,39 @@ public class KenkenActivity extends ActivityBase {
     private void bindView() {
         this.gridView = findViewById(R.id.gridview);
 
-            //premiere ligne pour test sur virtual device , 2e et 3e pour test sur terminal
+        //premiere ligne pour test sur virtual device , 2e et 3e pour test sur terminal
         //get_blocs_via_api();
-        ArrayList<Block> blocks = get_blocs();
+        ArrayList<Block> blocks = get_blocs(0);
         load_grille(blocks);
 
         this.raz_btn = findViewById(R.id.raz_btn);
         this.raz_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO raz du gridview
                 raz();
+            }
+        });
+
+        this.new_game_btn = findViewById(R.id.new_game_btn);
+        this.new_game_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new_game();
             }
         });
     }
 
     private void raz() {
-        ArrayList<Block> blocks = get_blocs();
+        ArrayList<Block> blocks = get_blocs(0);
+        this.blockAdapter = new BlockAdapter(this, blocks);
+        gridView.setAdapter(this.blockAdapter);
+        this.blockAdapter.notifyDataSetChanged();
+    }
+
+    private void new_game() {
+        //TODO sauvegarde partie en cours 'Game Over' --> api
+
+        ArrayList<Block> blocks = get_blocs(1);
         this.blockAdapter = new BlockAdapter(this, blocks);
         gridView.setAdapter(this.blockAdapter);
         this.blockAdapter.notifyDataSetChanged();
