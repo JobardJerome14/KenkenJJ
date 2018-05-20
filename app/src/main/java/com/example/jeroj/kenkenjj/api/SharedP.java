705dca962f1d216be3a742664ebc0fc24eab78a9
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-20 19:51:34 +0200

    [EVOL] ajout grille aux shared preferences

--- app/src/main/java/com/example/jeroj/kenkenjj/api/SharedP.java
+++ app/src/main/java/com/example/jeroj/kenkenjj/api/SharedP.java
@@ -3,6 +3,12 @@ package com.example.jeroj.kenkenjj.api;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.example.jeroj.kenkenjj.models.Grille;
+import com.example.jeroj.kenkenjj.ui.models.Block;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 public class SharedP {
     private Context context;
 
@@ -25,9 +31,37 @@ public class SharedP {
         return getSharedPref(UserPrefHelper.USER_PREF_DICO).getString(UserPrefHelper.USER_ID, "");
     }
 
+    public void setCurrentGrille(Grille grille) {
+        Gson gson = new Gson();
+        String json = gson.toJson(grille);
+
+        getSharedPref(UserPrefHelper.USER_PREF_DICO).edit()
+                .putString(UserPrefHelper.USER_CURRENT_GRILLE, json)
+                .apply();
+    }
+
+    public Grille getCurrentGrille() {
+        Gson gson = new Gson();
+        String json = getSharedPref(UserPrefHelper.USER_PREF_DICO).getString(UserPrefHelper.USER_CURRENT_GRILLE, "");
+        Grille grille = gson.fromJson(json, Grille.class);
+
+        return grille;
+    }
+/*
+    public void setCurrentIdGrille(Integer id_grille) {
+        getSharedPref(UserPrefHelper.USER_PREF_DICO).edit()
+                .putString(UserPrefHelper.USER_CURRENT_ID_GRILLE, Integer.toString(id_grille))
+                .apply();
+    }
+
+    public Integer getCurrentIdGrille() {
+        return Integer.parseInt(getSharedPref(UserPrefHelper.USER_PREF_DICO).getString(UserPrefHelper.USER_CURRENT_ID_GRILLE, ""));
+    }*/
 
     private class UserPrefHelper {
         final static String USER_PREF_DICO = "user_pref";
         final static String USER_ID = "user_id";
+        final static String USER_CURRENT_GRILLE = "user_grille";
+        final static String USER_CURRENT_ID_GRILLE = "user_current_id_grille";
     }
 }
