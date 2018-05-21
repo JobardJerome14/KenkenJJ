35b996e781fb85301b9cb745fd8c726d0d372217
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-21 14:02:30 +0200

    [EVOL] ajout écran Règles

---
+++ app/src/main/java/com/example/jeroj/kenkenjj/ui/RulesActivity.java
@@ -0,0 +1,15 @@
+package com.example.jeroj.kenkenjj.ui;
+
+import android.os.Bundle;
+
+import com.example.jeroj.kenkenjj.R;
+import com.example.jeroj.kenkenjj.ui.reusable.ActivityBase;
+
+public class RulesActivity extends ActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
