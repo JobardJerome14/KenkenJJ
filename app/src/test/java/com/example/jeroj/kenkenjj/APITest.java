bbafbd64327bc55d4ef5b05444a43f3af404b3de
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-24 19:58:33 +0200

    [TEST] creation APITests

---
+++ app/src/test/java/com/example/jeroj/kenkenjj/APITest.java
@@ -0,0 +1,42 @@
+package com.example.jeroj.kenkenjj;
+
+import com.example.jeroj.kenkenjj.api.IApi;
+import com.example.jeroj.kenkenjj.models.Grille;
+import com.example.jeroj.kenkenjj.models.RetourUpdate;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APITest {
+
+    private IApi iApi;
+
+    public APITest() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        // ip 10.0.2.2 special IP for localhost
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost").client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        this.iApi = retrofit.create(IApi.class);
+    }
+
+    public Call<Grille> get_grille (String user_id) {
+        // Create a call instance for looking up Retrofit contributors.
+        return this.iApi.getKenkenGrille(user_id);
+    }
+
+    public Call<RetourUpdate> update_ken_game (String user_id, Integer id_grille, Integer etat) {
+        return this.iApi.updKenGame(user_id, id_grille, etat);
+    }
+
+}
+
---
+++ app/src/test/java/com/example/jeroj/kenkenjj/APITests.java
@@ -0,0 +1,126 @@
+package com.example.jeroj.kenkenjj;
+
+import com.example.jeroj.kenkenjj.models.Grille;
+import com.example.jeroj.kenkenjj.models.RetourUpdate;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class APITests {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void test_get_grille() {
+
+        APITest apitest = new APITest();
+        Call<Grille> cm = apitest.get_grille("user_test");
+        try {
+            Response<Grille> rm = cm.execute();
+            assertEquals(36, rm.body().getBlocks().size());
+            assertNotEquals("0", String.valueOf(rm.body().getId_grille()));
+        } catch (IOException e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void test_update_ken_game_victory() {
+        APITest apitest = new APITest();
+        Call<Grille> cm = apitest.get_grille("user_test");
+        try {
+            Response<Grille> rm = cm.execute();
+            Call<RetourUpdate> cm2 = apitest.update_ken_game("user_test", rm.body().getId_grille(), 1);
+            try {
+                Response<RetourUpdate> rm2 = cm2.execute();
+                assertEquals("OK", rm2.body().getStatus());
+            } catch (IOException e) {
+                assertTrue(false);
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void test_update_ken_game_abandon() {
+        APITest apitest = new APITest();
+        Call<Grille> cm = apitest.get_grille("user_test");
+        try {
+            Response<Grille> rm = cm.execute();
+            Call<RetourUpdate> cm2 = apitest.update_ken_game("user_test", rm.body().getId_grille(), 0);
+            try {
+                Response<RetourUpdate> rm2 = cm2.execute();
+                assertEquals("OK", rm2.body().getStatus());
+            } catch (IOException e) {
+                assertTrue(false);
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void test_update_ken_game_no_user_id() {
+        APITest apitest = new APITest();
+        Call<Grille> cm = apitest.get_grille("user_test");
+        try {
+            Response<Grille> rm = cm.execute();
+            Call<RetourUpdate> cm2 = apitest.update_ken_game("", rm.body().getId_grille(), 1);
+            try {
+                Response<RetourUpdate> rm2 = cm2.execute();
+                assertEquals("KO", rm2.body().getStatus());
+            } catch (IOException e) {
+                assertTrue(false);
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Test
+    public void test_update_ken_game_no_id_grille() {
+        APITest apitest = new APITest();
+        Call<Grille> cm = apitest.get_grille("user_test");
+        try {
+            Response<Grille> rm = cm.execute();
+            Call<RetourUpdate> cm2 = apitest.update_ken_game("user_test", 0, 1);
+            try {
+                Response<RetourUpdate> rm2 = cm2.execute();
+                assertEquals("KO", rm2.body().getStatus());
+            } catch (IOException e) {
+                assertTrue(false);
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+}
