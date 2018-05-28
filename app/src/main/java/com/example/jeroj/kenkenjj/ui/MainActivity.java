fd65c98e4a571e8818810f7421f967224ab3975c
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-28 23:08:21 +0200

    [EVOL] premier event firebase

--- app/src/main/java/com/example/jeroj/kenkenjj/ui/MainActivity.java
+++ app/src/main/java/com/example/jeroj/kenkenjj/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import com.example.jeroj.kenkenjj.R;
 import com.example.jeroj.kenkenjj.api.SharedP;
 import com.example.jeroj.kenkenjj.ui.reusable.ActivityBase;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.util.UUID;
 
@@ -17,6 +18,9 @@ public class MainActivity extends ActivityBase {
     Button welcome_btn;
 
     SharedP sharedP;
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +38,22 @@ public class MainActivity extends ActivityBase {
     private void bindView() {
         this.tw_main = findViewById(R.id.tw_main);
 
+        this.mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         this.welcome_btn = findViewById(R.id.welcome_btn);
         this.welcome_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 play_ken();
+
             }
         });
     }
 
     public void play_ken() {
         navigate(KenkenActivity.class, null);
+        Bundle bundle = new Bundle();
+        bundle.putString("user_id", this.sharedP.getUserId());
+        mFirebaseAnalytics.logEvent("play_btn", bundle);
     }
 }
