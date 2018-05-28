077c40027a6b9c456f2aa800ef91fe98e5c80148
JobardJerome14 <38906242+JobardJerome14@users.noreply.github.com>
2018-05-28 22:50:26 +0200

    [EVOL] modif build gradle pour espresso test

--- app/src/androidTest/java/com/example/jeroj/kenkenjj/ui/MainActivityTestVictoire.java
+++ app/src/androidTest/java/com/example/jeroj/kenkenjj/ui/MainActivityTestVictoire.java
@@ -44,7 +44,7 @@ public class MainActivityTestVictoire {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(60000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -63,7 +63,7 @@ public class MainActivityTestVictoire {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(3582717);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
